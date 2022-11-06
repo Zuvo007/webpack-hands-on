d603144703a6423bf9a680520e2086dc633799b7
Zuvo007 <duttasubhabrata123@gmail.com>
2022-11-06 14:53:07 +0530

    file changed

--- src/text.js
+++ src/text.js
@@ -1,9 +1,8 @@
 export function hello() {
-    document.write("hello from webpack!!!!"); 
+    document.write("hello from webpack!!!!!"); 
 }
 
 export function changeH1(text) {
     document.querySelector('h1').innerText = text;
 
 }
-
